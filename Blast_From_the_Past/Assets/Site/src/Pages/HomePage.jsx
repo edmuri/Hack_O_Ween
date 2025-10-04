f95b867892d3c62ee9147170c900d77e7e59441f
Eddie <ed.murillo717@gmail.com>
2025-10-04 15:48:32 -0500

    adding a backdrop

--- Blast_From_the_Past/Assets/Site/src/Pages/HomePage.jsx
+++ Blast_From_the_Past/Assets/Site/src/Pages/HomePage.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Backdrop from "./Photos/backdrop.jpg";
+import "./HomePage.css";
 
 function HomePage(){
     return(
-        <div>
-            <p>Hello</p>
+        <div className="completeDiv">
+            <img src={Backdrop} className="backdrop"/>
+
+            <div className="mainDiv">
+                <p>Hello</p>
+            </div>
         </div>
     )
 }
