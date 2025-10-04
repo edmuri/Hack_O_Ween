20b3d2e8d5787de8ae4ce0c5a91e6432fd06662a
Eddie <ed.murillo717@gmail.com>
2025-10-04 12:35:03 -0500

    adding LinkedRooms challenge

--- LinkedRooms/Assets/backrooms.c
+++ LinkedRooms/Assets/backrooms.c
@@ -7,6 +7,10 @@ int main(){
     //node is now the start of the maze that you can enter and evaluate
     ListNode* node = CreateMaze();
 
-    printf("%s", "Maze Created!");
+    printf("%s", "Maze Created!\n");
+
+    DestroyMaze(node);
+    printf("%s","Maze Destroyed\n");
+    
     return 0;
 }
