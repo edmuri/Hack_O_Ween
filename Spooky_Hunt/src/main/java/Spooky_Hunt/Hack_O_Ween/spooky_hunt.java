e6ab7ae83c3b5f46bdf7ded8b9ee7e043fc40ead
Eddie <ed.murillo717@gmail.com>
2025-10-04 19:22:43 -0500

    adding Demonic Themonics documentation

--- Spooky_Hunt/src/main/java/Spooky_Hunt/Hack_O_Ween/spooky_hunt.java
+++ Spooky_Hunt/src/main/java/Spooky_Hunt/Hack_O_Ween/spooky_hunt.java
@@ -1,12 +1,30 @@
 package Spooky_Hunt.Hack_O_Ween;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Polygon;
+import javafx.scene.text.TextFlow;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 
 public class spooky_hunt extends Application {
+    Scene scene;
+    BorderPane root;
+
     @Override
     public void start(Stage stage) {
-        stage.setTitle("My JavaFX App");
+        BorderPane root = new BorderPane();
+        scene = new Scene(root, 500,700);
+        stage.setTitle("Spooky_Hunt");
         stage.show();
     }
 
