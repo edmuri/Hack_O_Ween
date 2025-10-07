9d4e5eeabde3ca03203c43f92e34fc35b77264fa
Eddie <ed.murillo717@gmail.com>
2025-10-07 15:16:04 -0500

    adding main functionality for flag population

--- Demonic_Themonics/Assets/Site/src/Pages/EmbeddedDiv.jsx
+++ Demonic_Themonics/Assets/Site/src/Pages/EmbeddedDiv.jsx
@@ -1,36 +1,74 @@
-import React from "react";
+import React, { createElement } from "react";
 import ReactDOM from "react-dom/client";
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 const EmbeddedDiv = ({activeTheme, onThemeChange, themes}) =>{
+    
+    async function make_divs() {
+        const temp = document.querySelector(".bigDiv");
+        if(temp!=null)
+            document.querySelectorAll(".bigDiv").forEach(e => e.remove());
+
+        const parent = document.createElement("div");
+        parent.setAttribute("class","bigDiv");
+        parent.setAttribute("hidden","");
+
+        const length = themeDetails[activeTheme].Divs.length;
+        for(let j = 0; j<length-1;j++){
+            for(let i=0;i<100;i++){
+                const div = document.createElement("div");
+                const p = document.createElement("p");
+                if(i==75 && activeTheme=="Scary"){
+                    p.textContent="flame{D00MI$C0MING}";
+                    p.setAttribute("class","flag");
+                }
+                else
+                    p.textContent=themeDetails[activeTheme].Divs[j].details;
+                div.appendChild(p);
+                parent.appendChild(div);
+            }
+        }
+        const main = document.querySelector(".mainDiv");
+        main.appendChild(parent);
+    };
+
     const themeDetails = {
         Default: {
             details:"Hello",
-            Divs:{
-                
-            }
+            Divs:[
+                {details:"fl"},
+                {details:"hell0"}
+            ]
         },
         Light:{
             details:"LIGHT",
-            Divs:{
-
-            }
+            Divs:[
+                {details:"fl"},
+                {details:"hell0"}
+            ]
         },
         Scary:{
             details:"AAAAHH",
-            Divs:{
-
-            }
+            Divs:[
+                {details:"fl"},
+                {details:"hell0"}
+            ]
         },
         Secret:{
-            details:"flame{DO0MI$C0MING}",
-            Divs:{
-
-            }
+            details:"NOOOO",
+            Divs:[
+                {details:"fl"},
+                {details:"hell0"}
+            ]
         }
     };
+
+     useEffect(() => {
+        make_divs();
+    }, [activeTheme]);
+
     return(
-        <div>
+        <div id="BOOOOOOO">
             <p hidden>{themeDetails[activeTheme].details}</p>
         </div>
     )
