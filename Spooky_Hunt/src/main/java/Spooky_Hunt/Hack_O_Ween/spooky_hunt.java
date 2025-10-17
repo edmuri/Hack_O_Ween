4270e7507fcac0f079868a5e513754fac60cfdaa
Eddie <ed.murillo717@gmail.com>
2025-10-16 21:03:58 -0500

    adding more Spooky_Hunt code

--- Spooky_Hunt/src/main/java/Spooky_Hunt/Hack_O_Ween/spooky_hunt.java
+++ Spooky_Hunt/src/main/java/Spooky_Hunt/Hack_O_Ween/spooky_hunt.java
@@ -28,7 +28,14 @@ public class spooky_hunt extends Application {
     Button submitButton;
     Text question;
     TextField answerField;
+
     String pumpkin1 = "/Spooky_Hunt/Hack_O_Ween/pumpkin_1.gif";
+    String banner_path = "/Spooky_Hunt/Hack_O_Ween/banner.gif";
+    // String bloody_banner_path = "/Spooky_Hunt/Hack_O_Ween/blood-drip.gif";
+    String pumpkin2_path = "/Spooky_Hunt/Hack_O_Ween/pumpkin_2.gif";
+    // String spikey_path = "/Spooky_Hunt/Hack_O_Ween/spikey.png";
+
+    static Scene congratsScene;
 
     static Vector<Question_Round> rounds = createQuestions();
 
@@ -44,8 +51,17 @@ public class spooky_hunt extends Application {
 
         Image pumpkin_cat = new Image(getClass().getResource(pumpkin1).toExternalForm());
         ImageView pumpkin_cat_view = new ImageView(pumpkin_cat);
-        HBox tmp = new HBox(startButton, pumpkin_cat_view);
-        starterPane = new BorderPane(tmp);
+
+        Image banner = new Image(getClass().getResource(banner_path).toExternalForm());
+        ImageView banner_view = new ImageView(banner);
+
+        VBox tmp = new VBox(banner_view, startButton);
+        HBox cat_container = new HBox(pumpkin_cat_view);
+        cat_container.setAlignment(Pos.BOTTOM_RIGHT);
+        tmp.setAlignment(Pos.CENTER);
+        starterPane = new BorderPane();
+        starterPane.setCenter(tmp);
+        starterPane.setBottom(cat_container);
         starterPane.setStyle("-fx-background-color:darkred;");
         startScene = new Scene(starterPane, 700,600);
 
@@ -83,11 +99,20 @@ public class spooky_hunt extends Application {
     }
 
     public static void updateScene(int spot){
-        System.out.println(spot);
         if(spot<4)
             stage.setScene(rounds.get(spot+1).getScene());
-        // else   
-        //     stage.setScene(congrats);
+        else   
+            stage.setScene(createCongratsScene());
+    }
+
+    public static Scene createCongratsScene(){
+        Text flag = new Text("flame{neeee3rd!}");
+        VBox tempBox = new VBox(flag);
+        tempBox.setAlignment(Pos.CENTER);
+        BorderPane tempPage = new BorderPane(flag);
+        Scene tmp = new Scene(tempPage,700,600);
+
+        return tmp;
     }
 }
 
@@ -96,6 +121,10 @@ class Question_Round{
         private String Answer;
         private int roundNum;
         private Scene scene;
+        private String spikey_path = "/Spooky_Hunt/Hack_O_Ween/spikey.png";
+        private String bloody_banner_path = "/Spooky_Hunt/Hack_O_Ween/bloody_drip.png";
+        private String cute_gruntilda_path = "/Spooky_Hunt/Hack_O_Ween/pixel_art.png";
+        private String gruntilda_path = "/Spooky_Hunt/Hack_O_Ween/gruntilda.png";
 
         Question_Round(int roundNum){
             this.roundNum=roundNum;
@@ -115,8 +144,8 @@ class Question_Round{
 
             Text questionArea = new Text(question);
             TextField answerBox = new TextField();
-            answerBox.setPrefWidth(50);
-            answerBox.setStyle("-fx-background-color:#394F3F;");
+            answerBox.setPrefWidth(500);
+            answerBox.setStyle("-fx-background-color:#394F3F;-fx-margin-left:50px;-fx-margin-right:50px;-fx-text-color:#000000;");
 
             Button submit = new Button("Submit");
             submit.setStyle("-fx-background-color:#2D5155;");
@@ -127,11 +156,72 @@ class Question_Round{
                 }
             });
 
+            VBox decoration = new VBox();
+
+            switch(this.roundNum){
+                //myers
+                case 0:
+                    break;
+                    //witch
+                case 1:
+                    Image gruntilda_image = new Image(getClass().getResource(gruntilda_path).toExternalForm());
+                    ImageView gruntilda_view = new ImageView(gruntilda_image);
+                    gruntilda_view.setFitHeight(300);
+                    gruntilda_view.setFitWidth(300);
+                    gruntilda_view.setPreserveRatio(true);
+
+                    Image cute_gruntilda = new Image(getClass().getResource(cute_gruntilda_path).toExternalForm());
+                    ImageView cute_gruntilda_view = new ImageView(cute_gruntilda);
+                    cute_gruntilda_view.setFitWidth(300);   
+                    cute_gruntilda_view.setFitHeight(200);    
+                    cute_gruntilda_view.setPreserveRatio(true);
+
+                    HBox tmpor = new HBox(gruntilda_view,cute_gruntilda_view);
+                    tmpor.setAlignment(Pos.CENTER);
+                    HBox.setHgrow(tmpor, Priority.NEVER);
+                    decoration.getChildren().add(tmpor);
+                    decoration.setMaxWidth(100);
+                    decoration.setMaxHeight(100);
+                    break;
+                    //friday
+                case 2:
+                    break;
+                    //spikey
+                case 3:
+                    Image spikey_image = new Image(getClass().getResource(spikey_path).toExternalForm());
+                    ImageView spikey_view = new ImageView(spikey_image);
+                    
+                    decoration.getChildren().add(spikey_view);
+                    decoration.setAlignment(Pos.CENTER);
+                    break;
+                    //carrie
+                case 4:
+                    Image bloody_banner_image = new Image(getClass().getResource(bloody_banner_path).toExternalForm());
+                    ImageView bloody_banner_view = new ImageView(bloody_banner_image);
+                    bloody_banner_view.fitWidthProperty().bind(decoration.widthProperty());
+                    bloody_banner_view.setFitHeight(200); 
+                    bloody_banner_view.setPreserveRatio(false);
+                    decoration.getChildren().add(bloody_banner_view);
+                    decoration.setAlignment(Pos.TOP_CENTER);
+                    break;
+                default:
+                    break;
+            }
+
+
             VBox Everything = new VBox(questionArea,answerBox, submit);
             Everything.setAlignment(Pos.CENTER);
+            answerBox.setMaxWidth(300);
+            VBox.setVgrow(answerBox, Priority.NEVER);
+            Everything.setSpacing(15);
+
+
             BorderPane inner = new BorderPane();
+            inner.setTop(decoration);
             inner.setCenter(Everything);
-            Everything.setStyle("-fx-background-color:#263F42;");
+           inner.setStyle("-fx-background-color:#263F42;");
+
+            BorderPane.setAlignment(Everything,Pos.CENTER);
             
             this.scene = new Scene(inner,700,600);
 
@@ -146,7 +236,6 @@ class Question_Round{
         }
 
         public boolean matches(String text){
-            System.out.println(Answer + " " + text);
             if(text.equals(Answer))
                 return true;
             else
