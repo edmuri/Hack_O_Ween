2d902044cc804cb4159928e2d599b69c88eb74b5
Eddie <ed.murillo717@gmail.com>
2025-10-24 14:42:44 -0500

    attempting to fix deployment

--- Domonic_Themonics/Assets/Site/Source/vite.config.js
+++ Domonic_Themonics/Assets/Site/Source/vite.config.js
@@ -2,6 +2,6 @@ import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    pluggins: [react()],
+    plugins: [react()],
     base:"/Hack-O-Ween/Domonic_Themonics/Assets/Site/"
 });
